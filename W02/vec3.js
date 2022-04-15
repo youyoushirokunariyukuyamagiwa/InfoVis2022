d57deec21d037a57c050f2adba7cce70f4f3e9a8
youyoushirokunariyukuyamagiwa <65272676+youyoushirokunariyukuyamagiwa@users.noreply.github.com>
2022-04-15 10:29:02 +0900

    Update vec3.js

--- W02/vec3.js
+++ W02/vec3.js
@@ -53,10 +53,50 @@ Vec3.prototype.cross = function( v )
     this.x = y * v.z - z * v.y;
     this.y = z * v.x - x * v.z;
     this.z = x * v.y - y * v.x;
+    
     return this;
 }
 
 Vec3.prototype.length = function()
 {
     return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+}
+
+Vec3.prototype.min() = function()
+{
+    var min = this.x;
+    if(this.y < min){
+        min = this.y;
+    }
+    if(this.z < min){
+        min = this.z;
+    }
+
+    return min;
+}
+
+Vec3.prototype.max() = function()
+{
+    var max = this.x;
+    if(this.y > max){
+        max = this.y;
+    }
+    if(this.z > max){
+        max = this.z;
+    }
+
+    return max;
+}
+
+Vec3.prototype.mid() = function()
+{
+    if(this.x > this.y){
+        if(this.z > this.x)return this.x;
+        else if(this.y > this.z)return this.y;
+        else return this.z;
+    }else if(this.y > this.x){
+        if(this.z > this.y)return this.y;
+        else if(this.x > this.z)return this.x;
+        else return this.z;
+    }
 }
